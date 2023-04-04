fade7830bed9ccccc58babe655815f0f4af6910e
Bahadur Khan Swati <57499169+BahadurKhan10@users.noreply.github.com>
2023-04-04 14:13:52 +0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,35 @@
 abstract class Vehicle{
-    constructor(
-        public _model: string,
-        public _yearAssembled: Date,
-        public _rented: "True" | "False", //can write Boolean
-        public _rentRate: number,
-
-
-    ){} 
+    private _model: string;
+    private _yearAssembled: string;
+    private _rented: boolean;
+    private _rentRate: number;
+    constructor(model:string, yearAssembled: string, rented:boolean, rentRate:number){
+        this._model=model,
+        this._yearAssembled=yearAssembled,
+        this._rented=rented,
+        this._rentRate=rentRate
+    } 
         //getters
-        public get model(){
+        get model(){
             return this._model
         }
 
-        public get yearAssembled(){
+        get yearAssembled(){
             return this._yearAssembled
         }
 
-        public get rented(){
+        get rented(){
             return this._rented
         }
 
-        public get rentRate(){
+        get rentRate(){
             return this._rentRate
         }
 
 
 
         //Setters
-        public set rented(newRented:"True"|"False"){
+        public set rented(newRented:boolean){
             this._rented=newRented
         }
 
@@ -35,132 +37,156 @@ abstract class Vehicle{
         abstract rentalRate(): number;
 
 
-        rent(){
-            if(this._rented=="False"){
-                console.log("Renting Vehicle...")
+        rent():string{
+            if(this._rented===false){
+                return "Renting Vehicle...\n Vechile rented successfully."
+
             }
-            console.log("This Vehicle is already rented")
+            else{
+            return "This Vehicle is already rented"}
         }
 
         return(){
-            if(this._rented=="True"){
-                console.log("Returning rented Vehicle")
-                this._rented="False"
+            if(this._rented===true){
+                console.log("Returning rented Vehicle \n Returned rented vechle successfully.") 
+                this._rented= false;
+                
             }
-            console.log("This Vehicle is not rented")
+            else{
+            return "This Vehicle is already rented"}
 
         }
 }
 
+// Sub classes for Vehicle types.
+
 class Car extends Vehicle{
+    private _color: string;
+    private _numberPlate: string;
+    private _capacity:number;
+
     constructor(
         model: string,
-        yearAssembled: Date,
-        rented: "True" | "False", //can write Boolean
+        yearAssembled: string,
+        rented: boolean,
         rentRate:number,
-        private _color: string,
-        private _numberPlate: string,
-        private _capacity:number
-    ){super(model,yearAssembled,rented,rentRate)}
+        color:string,
+        numberPlate:string,
+        capacity:number
+){
+    super(model,yearAssembled,rented,rentRate);
+    this._color=color,
+    this._numberPlate=numberPlate,
+    this._capacity=capacity
+}
     
     
-    public rentalRate(): number {
+    rentalRate(): number {
         return this.rentRate
     }
 
-    public get model(){
-        return this._model
-    }
-
-    public get color(){
+    get color(){
         return this._color
     }
 
-    public get capacity(){
+
+    get capacity(){
         return this._capacity
     }
 
-    public get numberPlate(){
+    get numberPlate(){
         return this._numberPlate
     }
 
-    public set numberPlate(newNumPlate:string){
+    set numberPlate(newNumPlate:string){
         this.numberPlate=newNumPlate
     }
 
 }
 
 class Truck extends Vehicle{
+    private _color: string;
+    private _numberPlate: string;
+    private _capacity:number;
+
     constructor(
         model: string,
-        yearAssembled: Date,
-        rented: "True" | "False", //can write Boolean
+        yearAssembled: string,
+        rented: boolean,
         rentRate:number,
-        private _color: string,
-        private _numberPlate: string,
-        private _capacity:number
-    ){super(model,yearAssembled,rented,rentRate)}
+        color:string,
+        numberPlate:string,
+        capacity:number
+){
+    super(model,yearAssembled,rented,rentRate);
+    this._color=color,
+    this._numberPlate=numberPlate,
+    this._capacity=capacity
+}
     
     
-    public rentalRate(): number {
+    rentalRate(): number {
         return this.rentRate
     }
 
-    public get model(){
-        return this._model
-    }
-
-    public get color(){
+    get color(){
         return this._color
     }
+
 
     get capacity(){
         return this._capacity
     }
 
-    public get numberPlate(){
+    get numberPlate(){
         return this._numberPlate
     }
-    
-    public set numberPlate(newNumPlate:string){
+
+    set numberPlate(newNumPlate:string){
         this.numberPlate=newNumPlate
     }
 
 }
 
 class Motorcycle extends Vehicle{
+    private _color: string;
+    private _numberPlate: string;
+    private _capacity:number;
+
     constructor(
         model: string,
-        yearAssembled: Date,
-        rented: "True" | "False", //can write Boolean
+        yearAssembled: string,
+        rented: boolean,
         rentRate:number,
-        private _color: string,
-        private _numberPlate: string,
-        private _capacity:number
-    ){super(model,yearAssembled,rented,rentRate)}
+        color:string,
+        numberPlate:string,
+        capacity:number
+){
+    super(model,yearAssembled,rented,rentRate);
+    this._color=color,
+    this._numberPlate=numberPlate,
+    this._capacity=capacity
+}
     
     
-    public rentalRate(): number {
+    rentalRate(): number {
         return this.rentRate
     }
 
-    public get model(){
-        return this._model
-    }
-
-    public get color(){
+    get color(){
         return this._color
     }
 
-    public get capacity(){
+
+    get capacity(){
         return this._capacity
     }
 
-    public get numberPlate(){
+    get numberPlate(){
         return this._numberPlate
     }
 
-    public set numberPlate(newNumPlate:string){
+    set numberPlate(newNumPlate:string){
         this.numberPlate=newNumPlate
     }
 
@@ -168,30 +194,30 @@ class Motorcycle extends Vehicle{
 
 // Main Program
 // Create instances of each type of vehicle
-const car = new Car("Audi", new Date(2020, 1, 1), "False", 50, "red", "ABC123", 5);
-const truck = new Truck("Ford", new Date(2019, 1, 1), "False", 100, "blue", "XYZ789", 1000);
-const motorcycle = new Motorcycle("Harley Davidson", new Date(2021, 1, 1), "False", 30, "black", "DEF456", 2);
+const car = new Car("Audi", "2020", false, 50, "red", "ABC123", 5);
+const truck = new Truck("Ford", "2019", false, 100, "blue", "XYZ789", 1000);
+const motorcycle = new Motorcycle("Harley Davidson", "2021", false, 30, "black", "DEF456", 2);
 
 // Rent and return Car
 console.log("Renting a car...");
-car.rent(); // Renting Vehicle...
+console.log(car.rent()); // Renting Vehicle...
 console.log("Trying to rent the same car again...");
-car.rent(); // This Vehicle is already rented
+console.log(car.rent()); // This Vehicle is already rented
 console.log("Returning the car...");
-car.return(); // Returning rented Vehicle
+console.log(car.return()); // Returning rented Vehicle
 console.log("Trying to return the same car again...");
-car.return(); // This Vehicle is not rented
+console.log(car.return()); // This Vehicle is not rented
 
 // Rent and return Truck
 console.log("Renting a truck...");
-truck.rent(); // Renting Vehicle...
+console.log(truck.rent()); // Renting Vehicle...
 console.log("Returning the truck...");
-truck.return(); // Returning rented Vehicle
+console.log(truck.return()); // Returning rented Vehicle
 
 // Rent and return Motorcycle
 console.log("Trying to return the motorcycle...");
-motorcycle.return(); // This Vehicle is not rented
+console.log(motorcycle.return()); // This Vehicle is not rented
 console.log("Renting the motorcycle...");
-motorcycle.rent(); // Renting Vehicle...
+console.log(motorcycle.rent()); // Renting Vehicle...
 console.log("Returning the motorcycle...");
-motorcycle.return(); // Returning rented Vehicle
+console.log(motorcycle.return()); // Returning rented Vehicle
